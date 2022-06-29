fee89eec5bbf8d9f49ac64c26434ccf5bbf9f743
Julian <julian.schmid@exxeta.com>
2022-06-29 09:51:14 +0200

    refactored table renderer test

--- test/table_renderer_test.py
+++ test/table_renderer_test.py
@@ -3,18 +3,19 @@ import unittest
 from csv_table.csv_table import CsvTable
 from table_renderer.table_renderer import _calculate_column_widths, _render_header, _render_data, draw_table
 
-SHOWRT_CSV = ["h1;h2;h 3",
-              "1;ä;text space",
-              "2;b;c",
-              "3;d;e"]
+
+SHORT_CSV = [["h1", "h2", "h 3"],
+             ["1", "ä", "text space"],
+             ["2", "b", "c"],
+             ["3", "d", "e"]]
 
 
 class TableRenderer(unittest.TestCase):
 
     def test_calculate_columns_width(self):
-        table = CsvTable(SHOWRT_CSV)
+        caption, body = SHORT_CSV[0], SHORT_CSV[1:]
 
-        width = _calculate_column_widths(table.caption, table.get_body())
+        width = _calculate_column_widths(caption, body=body)
 
         self.assertEqual(3, len(width))
         self.assertEqual(2, width[0])
@@ -22,9 +23,9 @@ class TableRenderer(unittest.TestCase):
         self.assertEqual(10, width[2])
 
     def test_render_header(self):
-        table = CsvTable(SHOWRT_CSV)
+        caption, body = SHORT_CSV[0], SHORT_CSV[1:]
 
-        table_str = draw_table(table.caption, table.get_body())
+        table_str = draw_table(caption, body)
 
         caption = table_str.split("\n")[0]
         seperator = table_str.split("\n")[1]
@@ -32,14 +33,15 @@ class TableRenderer(unittest.TestCase):
         self.assertEqual("--+--+----------+", seperator)
 
     def test_draw_table(self):
-        table = CsvTable(SHOWRT_CSV, page_length=2)
+        caption, body = SHORT_CSV[0], SHORT_CSV[1:]
 
-        table_str = draw_table(table.caption, table.get_body())
+        table_str = draw_table(caption, body)
 
-        self.assertEqual("h1|h2|h 3       |\n" + \
-                         "--+--+----------+\n" + \
-                         "1 |ä |text space|\n" + \
-                         "2 |b |c         |\n", table_str)
+        self.assertEqual("h1|h2|h 3       |\n" +
+                         "--+--+----------+\n" +
+                         "1 |ä |text space|\n" +
+                         "2 |b |c         |\n" +
+                         "3 |d |e         |\n", table_str)
 
 
 if __name__ == '__main__':
